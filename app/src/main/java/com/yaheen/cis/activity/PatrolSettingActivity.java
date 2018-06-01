7e30f190f0c1565cea20bced0bd1b4d59c52fc33
lyy603 <840567289@qq.com>
2018-06-01 17:15:18 +0800

    添加巡查开始界面

---
+++ app/src/main/java/com/yaheen/cis/activity/PatrolSettingActivity.java
@@ -0,0 +1,14 @@
+package com.yaheen.cis.activity;
+
+import android.os.Bundle;
+
+import com.yaheen.cis.R;
+
+public class PatrolSettingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patrol_setting);
+    }
+}
