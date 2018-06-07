3d618dfa4c7f1a3a665a8da5b6126add3cb49e46
lyy603 <840567289@qq.com>
2018-06-07 10:39:55 +0800

    添加地图聚合功能实现

--- app/src/main/java/com/yaheen/cis/activity/RecordMapActivity.java
+++ app/src/main/java/com/yaheen/cis/activity/RecordMapActivity.java
@@ -7,9 +7,12 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
@@ -20,7 +23,13 @@ import com.yaheen.cis.R;
 import com.yaheen.cis.adapter.DataServer;
 import com.yaheen.cis.adapter.RecordMapAdapter;
 import com.yaheen.cis.util.map.BDMapUtils;
+import com.yaheen.cis.util.map.cluster.Cluster;
+import com.yaheen.cis.util.map.cluster.ClusterItem;
+import com.yaheen.cis.util.map.cluster.ClusterManager;
 import com.yaheen.cis.util.map.MapViewLocationListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecordMapActivity extends Activity {
 
@@ -31,6 +40,8 @@ public class RecordMapActivity extends Activity {
     private MapView mapView = null;
 
     private BaiduMap mBaiduMap;
+
+    private ClusterManager<MyItem> mClusterManager;
 
     //判断地图是否是第一次定位
     boolean isFirstLoc = true;
@@ -81,6 +92,33 @@ public class RecordMapActivity extends Activity {
                 MyLocationConfiguration.LocationMode.NORMAL, true, null));
         BDMapUtils.setMapViewListener(new locationListener());
         setLocationData(BDMapUtils.getLocation());
+
+        // 定义点聚合管理类ClusterManager
+        mClusterManager = new ClusterManager<MyItem>(this, mBaiduMap);
+        // 添加Marker点
+        addMarkers();
+        // 设置地图监听，当地图状态发生改变时，进行点聚合运算
+        mBaiduMap.setOnMapStatusChangeListener(mClusterManager);
+        // 设置maker点击时的响应
+        mBaiduMap.setOnMarkerClickListener(mClusterManager);
+
+        mClusterManager.setOnClusterClickListener(new ClusterManager.OnClusterClickListener<MyItem>() {
+            @Override
+            public boolean onClusterClick(Cluster<MyItem> cluster) {
+                Toast.makeText(RecordMapActivity.this,
+                        "有" + cluster.getSize() + "个点", Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
+        mClusterManager.setOnClusterItemClickListener(new ClusterManager.OnClusterItemClickListener<MyItem>() {
+            @Override
+            public boolean onClusterItemClick(MyItem item) {
+                Toast.makeText(RecordMapActivity.this,
+                        "点击单个Item", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 
     private View getHeaderView() {
@@ -107,17 +145,17 @@ public class RecordMapActivity extends Activity {
         }
 
         MyLocationData locData = new MyLocationData.Builder().direction(100)
-                .latitude(mLoc.getLatitude()).longitude(mLoc.getLongitude()).build();
+                .latitude(39.914935).longitude(116.403119).build();
 
         // 设置定位数据
         mBaiduMap.setMyLocationData(locData);
 
         if (isFirstLoc) {
             isFirstLoc = false;
-            LatLng ll = new LatLng(mLoc.getLatitude(), mLoc.getLongitude());
+            LatLng ll = new LatLng(39.914935, 116.403119);
             MapStatus.Builder builder = new MapStatus.Builder();
             //设置地图层级（4-21）
-            builder.target(ll).zoom(19.0f);
+            builder.target(ll).zoom(17.0f);
             mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
         }
     }
@@ -143,6 +181,54 @@ public class RecordMapActivity extends Activity {
 //                builder.target(ll).zoom(19.0f);
 //                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
 //            }
+        }
+    }
+
+    /**
+     * 向地图添加Marker点
+     */
+    public void addMarkers() {
+        // 添加Marker点
+        LatLng llA = new LatLng(39.963175, 116.400244);
+        LatLng llB = new LatLng(39.942821, 116.369199);
+        LatLng llC = new LatLng(39.939723, 116.425541);
+        LatLng llD = new LatLng(39.906965, 116.401394);
+        LatLng llE = new LatLng(39.956965, 116.331394);
+        LatLng llF = new LatLng(39.886965, 116.441394);
+        LatLng llG = new LatLng(39.996965, 116.411394);
+
+        List<MyItem> items = new ArrayList<MyItem>();
+        items.add(new MyItem(llA));
+        items.add(new MyItem(llB));
+        items.add(new MyItem(llC));
+        items.add(new MyItem(llD));
+        items.add(new MyItem(llE));
+        items.add(new MyItem(llF));
+        items.add(new MyItem(llG));
+
+        mClusterManager.addItems(items);
+
+    }
+
+    /**
+     * 每个Marker点，包含Marker点坐标以及图标
+     */
+    public class MyItem implements ClusterItem {
+        private final LatLng mPosition;
+
+        public MyItem(LatLng latLng) {
+            mPosition = latLng;
+        }
+
+        @Override
+        public LatLng getPosition() {
+            return mPosition;
+        }
+
+        @Override
+        public BitmapDescriptor getBitmapDescriptor() {
+            return BitmapDescriptorFactory
+                    .fromResource(R.drawable.icon_gcoding);
         }
     }
 
