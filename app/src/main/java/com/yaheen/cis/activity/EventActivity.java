2292746400f95fd31db699e3d514bd186bb9cab6
lyy603 <840567289@qq.com>
2018-06-22 11:45:59 +0800

    修改版本号为1005

--- app/src/main/java/com/yaheen/cis/activity/EventActivity.java
+++ app/src/main/java/com/yaheen/cis/activity/EventActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
